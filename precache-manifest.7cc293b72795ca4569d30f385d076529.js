1c464435ebfb2fbbe6ee1c3f031301d6ff4e7202
Kushich <kushich@outlook.com>
2020-03-16 17:06:37 +0300

    Updates

---
+++ precache-manifest.7cc293b72795ca4569d30f385d076529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81cda061e19020d3889f7c32df0228f",
+    "url": "/marioplan/index.html"
+  },
+  {
+    "revision": "ba89d76066f7f5b144af",
+    "url": "/marioplan/static/css/main.81f2dcb9.chunk.css"
+  },
+  {
+    "revision": "a01b5db7aec65661b086",
+    "url": "/marioplan/static/js/2.ea67b4fc.chunk.js"
+  },
+  {
+    "revision": "6b9f671524e4817f679e05b83b5beb67",
+    "url": "/marioplan/static/js/2.ea67b4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba89d76066f7f5b144af",
+    "url": "/marioplan/static/js/main.b2aec4ab.chunk.js"
+  },
+  {
+    "revision": "909675d65c8ec0f2a27b",
+    "url": "/marioplan/static/js/runtime-main.8011f6ad.js"
+  }
+]);
